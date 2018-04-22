6d309b9802c9fecfece3f4d92b01508843a57bb4
xw <944750471@qq.com>
2018-04-22 14:14:31 +0800

    表单登录

--- src/main/java/com/xw/security/SecurityApplication.java
+++ src/main/java/com/xw/security/SecurityApplication.java
@@ -3,6 +3,9 @@ package com.xw.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 基础框架与表单登录
+ */
 @SpringBootApplication
 public class SecurityApplication {
 
